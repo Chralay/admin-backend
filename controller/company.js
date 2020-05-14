d3659d31769c82679b1f6e64947bebabc5690e3d
Clara-hy <921578272@qq.com>
2020-05-14 19:55:22 +0800

    第三次提交
    关于公司大部分都完成了，除了职位的删除还没写。
    已完成的：获取公司列表，更新公司审核属性isPass，获取某个公司对应的所有职位

--- controller/company.js
+++ controller/company.js
@@ -57,4 +57,69 @@ router.post('/updatePass', async(ctx, next)=>{
     }
 })
 
+router.get('/getByName', async (ctx, next) => {
+    const params = ctx.request.query
+    const res = await callCloudFn(ctx, 'company', {
+        $url: 'getCompany',
+        companyName: params.companyName
+    })
+    // console.log(res)
+    let data = []
+    if(res.resp_data){
+        data = JSON.parse(res.resp_data).data
+    }
+    // 以云存储保存的公司头像图片文件下载链接
+    let fileList = []
+    for (let i = 0, len = data.length; i < len; i++) {
+        if(!/^(https||http):\/\/.*/i.test(data[i].companyAvatar)){
+            // 不是以https或http开头
+            fileList.push({
+                fileid: data[i].companyAvatar,
+                max_age: 7200
+            })
+        }
+    }
+    const dlRes = await callCloudStorage.download(ctx, fileList)
+    let dlList = dlRes.file_list
+    for (let i = 0, len = data.length; i < len; i++) {
+        if(!/^(https||http):\/\/.*/i.test(data[i].companyAvatar)){
+            // 不是以https或http开头
+            data[i].companyAvatar = dlList[i].download_url
+        }
+    }
+    ctx.body = {
+        code: 20000,
+        data: data
+    }
+})
+
+router.get('/getJobsByName', async (ctx, next) => {
+    const params = ctx.request.query
+    const res = await callCloudFn(ctx, 'job-s', {
+        $url: 'jobNameInCom',
+        companyName: params.companyName
+    })
+    let data = []
+    let returnData = []
+    if(res.resp_data){
+        data = JSON.parse(res.resp_data).data
+        for(let i = 0, len = data.length; i < len; i++){
+            returnData.push({
+                jobId: data[i]._id,
+                jobDetail: data[i].detail,
+                jobName: data[i].jobName,
+                workPlace: data[i].workPlace,
+                publisherName: data[i].publisher.publisherName.slice(0,3),
+                publisherPosition: data[i].publisher.publisherName.slice(3),
+                isStop: data[i].isStop ? data[i].isStop : false
+            })
+        }
+    }
+    // console.log(returnData)
+    ctx.body = {
+        code: 20000,
+        data: returnData
+    }
+})
+
 module.exports = router
